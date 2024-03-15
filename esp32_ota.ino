46c98a2886078cde064d86ff83ef8ceb0f6a431c
vinod reddy <vinod.kumar@cybernetyx.com>
2024-03-15 16:45:36 +0530

    second_time2.1

--- esp32_ota.ino
+++ esp32_ota.ino
@@ -4,15 +4,18 @@
 #include <WiFiClientSecure.h>
 #include "cert.h"
 
-const char * ssid = "home_wifi";
-const char * password = "helloworld";
+const char * ssid = "CNXPRT";
+const char * password = "Cyber@123";
+
+#define LED_BUILTIN 2
 
 
 String FirmwareVer = {
-  "2.2"
+  "2.1"
 };
-#define URL_fw_Version "https://raw.githubusercontent.com/programmer131/ESP8266_ESP32_SelfUpdate/master/esp32_ota/bin_version.txt"
-#define URL_fw_Bin "https://raw.githubusercontent.com/programmer131/ESP8266_ESP32_SelfUpdate/master/esp32_ota/fw.bin"
+#define URL_fw_Version "https://raw.githubusercontent.com/Vinodreddy100/OTA/blob/main/bin_version.txt"
+
+#define URL_fw_Bin "https://raw.githubusercontent.com/Vinodreddy100/OTA/blob/main/fw.bin"
 
 //#define URL_fw_Version "http://cade-make.000webhostapp.com/version.txt"
 //#define URL_fw_Bin "http://cade-make.000webhostapp.com/firmware.bin"
